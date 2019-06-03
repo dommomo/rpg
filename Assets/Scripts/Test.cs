197411597bc937ed6dc8396de678590604057e0d
Dom Ryan <dommomo@gmail.com>
2019-06-03 15:32:11 +0930

    test line of code

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,9 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //test msg
+        private void UpdateMe() {
+            
+        }
     }
 
     // Update is called once per frame
